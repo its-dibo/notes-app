e7e6c20de5f9f2fb514e091a4fe38b02c53ea69e
Dibo <sh.eldeeb.2010@gmail.com>
2023-03-12 20:01:04 +0200

    fix: db path

--- database.ts
+++ database.ts
@@ -1,6 +1,6 @@
 import sqlite3 from "sqlite3";
 import { resolve } from "node:path";
 
-let db = new sqlite3.Database(resolve(__dirname, "../db.sqlite"));
+let db = new sqlite3.Database(resolve(__dirname, "./db.sqlite"));
 
 export default db;
