2c1de3ca9e0574b5d9cf11fd6323bb87d81cf9ba
Dibo <sh.eldeeb.2010@gmail.com>
2023-03-12 19:57:26 +0200

    feat: routes/send-notes

--- src/routes/v1/send-notes.ts
+++ src/routes/v1/send-notes.ts
@@ -1,7 +1,35 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
+import db from "~database";
 
+export class User {
+  id: number;
+  name: string;
+  picture: string;
+}
 
 /**
  * sends a note to one or multiple users with specific type
  */
-export default function(req:Request, res:Response){}
+export default function (req: Request, res: Response) {
+  
+  let { users, noteType=1, title, body, sentBy } = req.body;
+
+  // validate the body schema before processing
+  if(users?.length<1)res.status(500).json({error:`select the users to send the note`})
+  else if(!title || title.trim()==="")res.status(500).json({error:`the title is required`})
+  else if(!body || body.trim()==="")res.status(500).json({error:`cannot send an empty note`})
+  else{
+    users.map((user: User) => {
+      if(!user.id)return;
+      
+      db.run(`
+      INSERT INTO notes(title, body, note_type, sent_by) 
+      VALUES ("${title}", "${body}"), "${noteType}", "${user.id}"`);
+    });
+  
+    // todo: confirm that all insertions are successful
+    res.json({status:"ok"})
+  }
+
+  
+}
