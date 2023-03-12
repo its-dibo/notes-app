2df8599261d73b7d0b9dfca12103e587aff8ef8d
Dibo <sh.eldeeb.2010@gmail.com>
2023-03-12 19:57:08 +0200

    code: lint

--- database.ts
+++ database.ts
@@ -1,10 +1,6 @@
-import sqlite3 from 'sqlite3'
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import sqlite3 from "sqlite3";
+import { resolve } from "node:path";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+let db = new sqlite3.Database(resolve(__dirname, "../db.sqlite"));
 
-let db = new sqlite3.Database(resolve(__dirname,'../db.sqlite'));
-
-export default db
-
+export default db;
