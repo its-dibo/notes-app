71d48c584d9842914d9f0a81188fe8520b20b254
Dibo <sh.eldeeb.2010@gmail.com>
2023-03-13 15:27:07 +0200

    fix: insert default values to the database

--- src/models/install.ts
+++ src/models/install.ts
@@ -1,15 +1,16 @@
-import db from '~database'
+import db from "~database";
 
 // the media files is stored in /files/$note_id/$file_path
-// relations: 
+// relations:
 //   - note_type -> note_types
 //   - sent_by -> entities
 db.run(`CREATE TABLE IF NOT EXISTS notes (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
+   	user INTEGER NOT NULL,
    	title TEXT NOT NULL,
    	body TEXT NOT NULL,
 	note_type INTEGER DEFAULT 1,
-	sent_by INTEGER
+	sent_by INTEGER NOT NULL
     )`);
 
 db.run(`CREATE TABLE IF NOT EXISTS entities (
@@ -18,12 +19,18 @@ db.run(`CREATE TABLE IF NOT EXISTS entities (
    	picture TEXT
 )`);
 
-
 db.run(`CREATE TABLE IF NOT EXISTS note_types (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
    	name TEXT NOT NULL UNIQUE,
-    disabled INTEGER
+    disabled INTEGER default 0
 )`);
 
+db.run(`
+   INSERT INTO note_types (name, disabled) VALUES
+   ("normal", 0),
+   ("alert", 0),
+   ("reminder", 0),
+   ("disabled_example", 1)
+`);
 
 // db.close();
