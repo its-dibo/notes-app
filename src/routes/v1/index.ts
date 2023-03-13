56ee71c20a4cf7a546a74b546ffd9a4407f1167c
Dibo <sh.eldeeb.2010@gmail.com>
2023-03-13 15:28:42 +0200

    code: log info about the request

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,14 +1,17 @@
-import { Router } from 'express';
+import { Router, Request, Response, NextFunction } from "express";
 import sendNotesRoute from "./send-notes";
 import listNotesRoute from "./list-notes";
 import authMiddleware from "./middlewares/auth.middleware";
 
-// todo: setup authentication middleware
 let router = Router({ mergeParams: true });
 
-router.use(authMiddleware)
-router.post('/send',sendNotesRoute)
-router.get('/list',listNotesRoute)
+router.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(`api v1 is loaded ${req.method}: ${req.url}`);
+  next();
+});
+router.use(authMiddleware);
+router.post("/send", sendNotesRoute);
+router.get("/list/all", listNotesRoute);
+router.get("/list/:userId", listNotesRoute);
 
-export default router
-
+export default router;
